0aba522c534dfaf49b81e43308638bc301f72e0e
niu541412 <niu541412@gmail.com>
2025-05-03 17:17:26 +0800

    test2222

--- utils/github/install-macos.sh
+++ utils/github/install-macos.sh
@@ -23,7 +23,7 @@ rm -rf \
 
 brew tap copyq/kde utils/github/homebrew/
 
-if [[ $(uname -m) == 'x86_64' ]]; then
+if [[ $(uname -m) == 'x86_64_2' ]]; then
     qt_minimum_target=13.0
     curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/refs/heads/master/Formula/q/qt.rb
     sed -i.bak "s|-DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}\.0|-DCMAKE_OSX_DEPLOYMENT_TARGET=${qt_minimum_target}|g" qt.rb
