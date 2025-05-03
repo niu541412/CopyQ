5a2323c803844dc67d4e1d948cc157b629a34e91
niu541412 <niu541412@gmail.com>
2025-05-03 18:00:46 +0800

    test CLT

--- utils/github/install-macos.sh
+++ utils/github/install-macos.sh
@@ -23,17 +23,23 @@ rm -rf \
 
 brew tap copyq/kde utils/github/homebrew/
 
-if [[ $(uname -m) == 'x86_64_2' ]]; then
+echo "--------- $(xcrun --show-sdk-path) ---------"
+
+if [[ $(uname -m) == 'x86_64' ]]; then
     qt_minimum_target=13.0
     curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/refs/heads/master/Formula/q/qt.rb
     sed -i.bak "s|-DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}\.0|-DCMAKE_OSX_DEPLOYMENT_TARGET=${qt_minimum_target}|g" qt.rb
     mv qt.rb.bak qt.rb
     brew install --build-from-source --formula ./qt.rb
+    xcoed-select --switch /Library/Developer/CommandLineTools
+    rm -rf /Applications/Xcode.app
     brew uninstall vulkan-headers vulkan-loader molten-vk node pkgconf
 else
     brew install qt@6
 fi
 
-brew install --verbose \
+echo "+++++++++ $(xcrun --show-sdk-path) +++++++++"
+
+brew install \
     copyq/kde/kf6-knotifications \
     copyq/kde/kf6-kstatusnotifieritem
