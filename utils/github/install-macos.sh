2fdc5c4eeb28de37ff11fc5dfdf96014ba6b7a2f
niu541412 <niu541412@gmail.com>
2025-05-08 21:19:48 +0800

    ss29

--- utils/github/install-macos.sh
+++ utils/github/install-macos.sh
@@ -31,7 +31,8 @@ if [[ $BUILDNAME == 'macOS old' ]]; then
     qt_minimum_target=12.0
     curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/refs/heads/master/Formula/q/qt.rb
     sed -i.bak "s|-DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}\.0|-DCMAKE_OSX_DEPLOYMENT_TARGET=${qt_minimum_target}|g" qt.rb
-    mv qt.rb.bak qt.rb
+    awk '{print}/-DQT_FEATURE_ffmpeg=OFF/{print "\t-DCMAKE_FIND_FRAMEWORK=NEVER"}' qt.rb.bak > qt.rb
+    # mv qt.rb.bak qt.rb
     brew install --build-from-source --formula ./qt.rb
     brew uninstall vulkan-headers vulkan-loader molten-vk node
     rm -r /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/A
