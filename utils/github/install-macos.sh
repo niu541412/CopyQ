c7491e1fe8cf23fc4e97d892f3abdc0045894a54
niu541412 <niu541412@gmail.com>
2025-05-09 20:19:36 +0800

    new test7

--- utils/github/install-macos.sh
+++ utils/github/install-macos.sh
@@ -28,15 +28,12 @@ if [[ $BUILDNAME == 'macOS old' ]]; then
     # mkdir qt-bak
     # cp -r /usr/local/Cellar/qt qt-bak/
     # brew uninstall qt@6
-    qt_minimum_target=12.0
+    
     curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/refs/heads/master/Formula/q/qt.rb
-    # sed -i.bak "s|-DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}\.0|-DCMAKE_OSX_DEPLOYMENT_TARGET=${qt_minimum_target}|g" qt.rb
-    # mv qt.rb.bak qt.rb
-    sed -i.bak "s|-DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}\.0|-DCMAKE_OSX_DEPLOYMENT_TARGET=${qt_minimum_target}|g" qt.rb
-    awk '{print}/-DQT_FEATURE_ffmpeg=OFF/{print "\t-DCMAKE_FIND_FRAMEWORK=LAST"}' qt.rb.bak > qt.rb
-    # mv qt.rb.bak qt.rb
+    patch qt.rb utils/github/qt.rb.patch
     brew install --build-from-source --formula ./qt.rb
     brew uninstall vulkan-headers vulkan-loader molten-vk node
+
     # rm -r /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/A
     # mv qt-bak/qt/6.9.0/lib/QtGui.framework/Versions/A /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/
 else
