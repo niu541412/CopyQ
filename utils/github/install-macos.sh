68fe1775168c08e1af05806861b2cf6743c03927
niu541412 <niu541412@gmail.com>
2025-05-09 23:15:05 +0800

    new test9

--- utils/github/install-macos.sh
+++ utils/github/install-macos.sh
@@ -24,18 +24,18 @@ rm -rf \
 brew tap copyq/kde utils/github/homebrew/
 
 if [[ $BUILDNAME == 'macOS old' ]]; then
-    brew install qt@6
-    mkdir qt-bak
-    cp -r /usr/local/Cellar/qt qt-bak/
-    brew uninstall qt@6
+    # brew install qt@6
+    # mkdir qt-bak
+    # cp -r /usr/local/Cellar/qt qt-bak/
+    # brew uninstall qt@6
     
     curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/refs/heads/master/Formula/q/qt.rb
     patch qt.rb utils/github/qt.rb.patch
     brew install --build-from-source --formula ./qt.rb
     brew uninstall vulkan-headers vulkan-loader molten-vk node
 
-    rm -r /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/A
-    mv qt-bak/qt/6.9.0/lib/QtGui.framework/Versions/A /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/
+    # rm -r /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/A
+    # mv qt-bak/qt/6.9.0/lib/QtGui.framework/Versions/A /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/
 else
     brew install qt@6
 fi
