1b07dc12ea1d0f200137b8ef14f0c352d8cd4cf2
niu541412 <niu541412@gmail.com>
2025-05-10 17:08:57 +0800

    find png ~~~

--- utils/github/install-macos.sh
+++ utils/github/install-macos.sh
@@ -29,11 +29,26 @@ if [[ $BUILDNAME == 'macOS old' ]]; then
     # cp -r /usr/local/Cellar/qt qt-bak/
     # brew uninstall qt@6
     
+    ### libpng
+    find /usr/local /opt /usr -name "libpng*.dylib" 2>/dev/null
+    pkg-config --modversion libpng
+    pkg-config --cflags libpng
+    pkg-config --libs libpng
+    ###
+
     curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/refs/heads/master/Formula/q/qt.rb
     patch qt.rb utils/github/qt.rb.patch
     brew install --build-from-source --formula ./qt.rb --debug
     brew uninstall vulkan-headers vulkan-loader molten-vk node
     cat /private/tmp/qt*202505*/qt*/qtbase/src/3rdparty/libpng/png.c|grep -A3 libpng
+
+    ### libpng
+    find /usr/local /opt /usr -name "libpng*.dylib" 2>/dev/null
+    pkg-config --modversion libpng
+    pkg-config --cflags libpng
+    pkg-config --libs libpng
+    otool -L /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/A/QtGui
+    ###
 
     # rm -r /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/A
     # mv qt-bak/qt/6.9.0/lib/QtGui.framework/Versions/A /usr/local/Cellar/qt/6.9.0/lib/QtGui.framework/Versions/
